446b45cea6c5a54f321088ce8080ba7b06fc2cbe
maladesov <m.zaharenkov@mail.ru>
2025-05-10 19:01:36 +0300

    👽️ Указание S3 Credentials для хакатонов и создание бакета hackathons

--- minio/entrypoint.sh
+++ minio/entrypoint.sh
@@ -6,4 +6,6 @@ echo Starting autoconfig...
 mc alias set local http://s3:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 mc mb --ignore-existing local/avatars
+mc mb --ignore-existing local/hackathons
+
 mc anonymous set download local/avatars
